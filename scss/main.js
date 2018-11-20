099a2192542807ff2b2ad937b5f2f9263e17a40f
Chinara Gakhramanova <chinarachinara2001@mail.ru>
2018-11-20 18:30:03 +0300

    nava

---
+++ scss/main.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".nav__burger-button");
+const fixedMenu = document.querySelector(".burgerclick");
+const closeMenu = document.querySelector("burgerclick__close");
+
+
+    hamburger.addEventListener('click', function () {
+    fixedMenu.classList.add('burgerclick__open');
+    document.body.classList.add('scroll-block');
+    hamburger.classList.add("nav__burger-button__active");
+    fixedmenu.classList.add("burgerclick_open");
+
+  });
+
+  fixedMenu.addEventListener('click', function () {
+    fixedMenu.classList.remove('burgerclick__open');
+    document.body.classList.remove('scroll-block');
+    
+    hamburger.classList.remove("nav__burger-button__active");
+    fixedmenu.classList.remove("burgerclick_open");
+  });
+
+
+
