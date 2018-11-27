6d037f1b3f71836125ef58912485506b71c1189a
Chinara Gakhramanova <chinarachinara2001@mail.ru>
2018-11-27 20:38:59 +0300

    form

--- scripts/main.js
+++ scripts/main.js
@@ -241,3 +241,101 @@ function translate(pos) {
 arrows.addEventListener('click', buttonClick);
 
 window.addEventListener('load', calcWidthList);
+
+
+////////форма////////
+ 
+const myForm = document.querySelector ('.form');
+const sendButton = document.querySelector('.payment__button');
+
+sendButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  if (validateForm(myForm)) {
+      const data = {
+        name: myForm.elements.name.value,
+        phone: myForm.elements.phone.value,
+        street: myForm.elements.street.value,
+        house: myForm.elements.house.value,
+        call: myForm.elements.call.value,
+        floor: myForm.elements.floor.value,
+        comment: myForm.elements.comment.value,
+        apartment: myForm.elements.apartment.value,
+        number: myForm.elements.number.value,
+      };
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open( 'POST', 'https://webdev-api.loftschool.com/sendmail');
+    xhr.send(JSON.stringify(data));
+    xhr.addEventListener('load', function() {
+      if(xhr.response.status) {
+        console.log("Все ok!");
+        }
+    });
+  }
+});
+function validateForm(form){
+  let valid = true;
+
+  if (!validateField(form.elements.name)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.phone)) {
+    valid = false;
+  }
+  if (!validateField(form.elements.comment)) {
+    valid = false; 
+  }
+  if (!validateField(form.elements.street)) {
+    valid = false; 
+  }
+  if (!validateField(form.elements.house)) {
+    valid = false; 
+  }
+  if (!validateField(form.elements.floor)) {
+    valid = false; 
+  }
+  if (!validateField(form.elements.number)) {
+    valid = false; 
+  }
+  if (!validateField(form.elements.call)) {
+    valid = false; 
+  }
+  if (!validateField(form.elements.apartment)) {
+    valid = false; 
+  }
+  return valid;
+}
+
+function validateField(field) {
+  if (!field.checkValidity()) {
+    field.nextElementSibling.textContent = field.validationMessage;
+    return false;
+  } else {
+    field.nextElementSibling.textContent = "";
+    return true;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
